9095ac2dcceb0d302804567e25e64205a6d0bff4
chendawei <chendawei>
2019-10-25 10:42:07 +0800

    非必要的给默认值 配置化

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ VOLUME /tmp
 WORKDIR /home
 ADD  ./target/gateway-server-0.0.3-SNAPSHOT.jar app.jar
 
-ENTRYPOINT exec java -server -Xms1024m -Xmx1024m -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar app.jar  --server.session.timeout=300 --spring.cloud.config.profile=dev
+ENTRYPOINT exec java -server -Xms1024m -Xmx1024m -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar app.jar  --server.session.timeout=30000 --spring.cloud.config.profile=dev
 
 EXPOSE 9003
--- src/main/java/yb/ecp/fast/infra/GatewayServerApplication.java
+++ src/main/java/yb/ecp/fast/infra/GatewayServerApplication.java
@@ -1,10 +1,13 @@
 package yb.ecp.fast.infra;
 
+import org.apache.catalina.Context;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
@@ -15,11 +18,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import yb.ecp.fast.infra.infra.eureka.EurekaDeregister;
-import yb.ecp.fast.infra.infra.monitor.MemoryMonitor;
 import yb.ecp.fast.infra.util.StringUtil;
 
 import java.util.HashSet;
@@ -39,27 +40,35 @@ public class GatewayServerApplication {
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer(@Value("${fast.webfront.error-page.notfound: }") final String a) {
 
-        return new EmbeddedServletContainerCustomizer() {
+        EmbeddedServletContainerCustomizer c = new EmbeddedServletContainerCustomizer() {
             @Override
             public void customize(ConfigurableEmbeddedServletContainer container) {
                 if (!StringUtil.isNullOrSpace(String.valueOf(a))) {
                     ErrorPage localErrorPage = new ErrorPage(HttpStatus.NOT_FOUND, a);
                     Set<ErrorPage> s = new HashSet<ErrorPage>();
                     s.add(localErrorPage);
+                    ((TomcatEmbeddedServletContainerFactory) container).addContextCustomizers(
+                            new TomcatContextCustomizer() {
+                                @Override
+                                public void customize(Context context) {
+                                    context.setUseHttpOnly(false);
+                                }
+                            });
                     container.setErrorPages(s);
                 }
             }
         };
+        return c;
     }
 
-    @Bean
-    public MemoryMonitor memoryMonitor() {
-        return new MemoryMonitor() {
-            @Scheduled(fixedRate = 300000L)
-            public void scheduleMonitor() {
-            }
-        };
-    }
+//    @Bean
+//    public MemoryMonitor memoryMonitor() {
+//        return new MemoryMonitor() {
+//            @Scheduled(fixedRate = 300000L)
+//            public void scheduleMonitor() {
+//            }
+//        };
+//    }
 
     @Bean(initMethod = "showDeregisterInfo", destroyMethod = "deregister")
     public EurekaDeregister eurekaDeregister(EurekaRegistration a, EurekaServiceRegistry a2, EurekaClientConfigBean a3) {
@@ -67,7 +76,7 @@ public class GatewayServerApplication {
     }
 
     @Bean
-    public DefaultCookieSerializer defaultCookieSerializer(@Value("${server.session.cookie.name:SESSION}") String a) {
+    public DefaultCookieSerializer defaultCookieSerializer(@Value("${server.session.cookie.name:BJ_SESSION}") String a) {
         DefaultCookieSerializer tmp7_4 = new DefaultCookieSerializer();
         tmp7_4.setCookieName(a);
         return tmp7_4;
